16bdcb9fd6d5b0d5edac9ece1c8db52999c2384b
Kairon <34685175+kaironlucas@users.noreply.github.com>
2025-06-05 22:29:11 -0300

    Ajuste nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "../src/pages/Home"
 import Layout from "./Layout";
 import Produtos from "./pages/ProductListing";
@@ -7,14 +7,16 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="produtos" element={<Produtos />} />
-        <Route path="produto/:id" element={<Produto />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <HashRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="produtos" element={<Produtos />} />
+                <Route path="produto/:id" element={<Produto />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Route>
+        </Routes>
+    </HashRouter>
   );
 }
 
