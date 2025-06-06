c9505179d908dd9c6514749b1f04131e8d9d4af9
Kairon <34685175+kaironlucas@users.noreply.github.com>
2025-06-05 23:19:04 -0300

    Ajuste nas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
-    <HashRouter basename="/projeto-digital-store">
+    <HashRouter>
         <Routes>
-            <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Layout />}/>
                 <Route index element={<Home />} />
                 <Route path="produtos" element={<Produtos />} />
                 <Route path="produto/:id" element={<Produto />} />
                 <Route path="*" element={<NotFoundPage />} />
-            </Route>
         </Routes>
     </HashRouter>
   );
 }
+
--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,5 @@ import './index.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+    <App />
 )
